3545f6d62117186334459425d73cd529e835827e
51pennys <s51pennys@gmail.com>
2022-06-15 12:52:48 +0200

    404 page

--- angular-restaurant-app/src/app/app.module.ts
+++ angular-restaurant-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ErrorPage404Component } from './error-page404/error-page404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPage404Component
   ],
   imports: [
     BrowserModule,
