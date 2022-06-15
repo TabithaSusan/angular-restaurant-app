ce5d0490488de59a4f9946b175d1057d867fb1a5
51pennys <s51pennys@gmail.com>
2022-06-15 14:09:38 +0200

    Contact

--- angular-restaurant-app/src/app/app.module.ts
+++ angular-restaurant-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorPage404Component } from './error-page404/error-page404.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ErrorPage404Component,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
