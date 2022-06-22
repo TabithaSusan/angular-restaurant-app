b75197550e17755b6e318af5bb62cf195de655c8
TabithaSusan <95419252+TabithaSusan@users.noreply.github.com>
2022-06-22 21:38:36 +0200

    Säubern von code

--- angular-restaurant-app/src/app/book-table/booktable.php
+++ angular-restaurant-app/src/app/book-table/booktable.php
@@ -5,8 +5,9 @@ $PartySize = $_POST['$PartySize'];
 $ResTime = $_POST['$ResTime'];
 $ResName = $_POST['ResName'];
 $ResEmail = $_POST['$ResEmail'];
+$ResTelephone = $_POST['ResTelephone'];
 
-$sql = "INSERT INTO 'reservation' ('Date', 'PartySize', 'Time', 'Name', 'Email') VALUES ('$startDate', '$PartySize', '$ResTime', '$ResName', '$ResEmail')";
+$sql = "INSERT INTO 'reservation' ('Date', 'PartySize', 'Time', 'Name', 'Email', 'Telephone') VALUES ('$startDate', '$PartySize', '$ResTime', '$ResName', '$ResEmail', '$ResTelephone')";
 
 $rs = mysqli_query ($con, $sql);
 
