82b4121dbbe321701e60f1ffc7be749021cd29eb
Jim Hawley <38258807+jehaw@users.noreply.github.com>
2022-07-19 16:56:04 +0200

    Added notes for processing data

--- angular-restaurant-app/server.js
+++ angular-restaurant-app/server.js
@@ -1,8 +1,19 @@
- // set up ======================== 
+// Recommendation: Declare var con once if the connection will be reused repeatedly throughout the script. Ensure to test performance but it should not
+// make a significant hit
+
+// Add a new variable that is globally accessible that contains retrieved information that is required app wide, for instance, menu 
+
+// set up ======================== 
  var express = require('express');
  var app = express(); // create our app w/ express 
  var path = require('path');
  var mysql = require('mysql');
+
+// Example variable result holder
+
+ var menu = null;
+ var menuFields = null;
+
 const { isFromDtsFile } = require('@angular/compiler-cli/src/ngtsc/util/src/typescript');
 
  bodyParser = require('body-parser');
@@ -38,6 +49,7 @@ const { isFromDtsFile } = require('@angular/compiler-cli/src/ngtsc/util/src/type
       con.connect(function(err)){
             if(err) throw err;
             console.log("Connected");
+            // Invalid UPDATE SQL, "UPDATE kunden SET xxx = ?? WHERE id = ??";
             con.query("UPDATE * FROM kunden",
             function(error,results,fields){
                   if(error) throw error;
@@ -78,6 +90,16 @@ const { isFromDtsFile } = require('@angular/compiler-cli/src/ngtsc/util/src/type
             })
         }
         )
+       // After retrieving the data from the server, assign the variable menu the return value so it can be accessed
+       // menu = results (not sure what it would be called
+       // menuFields = fields
+       
+       // Trick! add the following line to the code and then set the debug breakpoint on this line
+       
+       var a = 1;
+       
+       // When the debugger stops at the breakbpoint, check the values and structure of results and fields to see what it is and how it is built
+       // Doing so will let you know how to access the info in your script
      })
     
 
