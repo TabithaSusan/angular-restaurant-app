3956fb3b095e0e72d4bb22359b9d25f3ba626772
TabithaSusan <95419252+TabithaSusan@users.noreply.github.com>
2022-07-19 22:39:05 +0200

    Kaputte Food Menu austauschen

--- angular-restaurant-app/src/app/food-menu/food-menu.component.spec.ts
+++ angular-restaurant-app/src/app/food-menu/food-menu.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { FoodmenuComponent } from './foodmenu.component';
+import { FoodMenuComponent } from './food-menu.component';
 
-describe('FoodmenuComponent', () => {
-  let component: FoodmenuComponent;
-  let fixture: ComponentFixture<FoodmenuComponent>;
+describe('FoodMenuComponent', () => {
+  let component: FoodMenuComponent;
+  let fixture: ComponentFixture<FoodMenuComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FoodmenuComponent ]
+      declarations: [ FoodMenuComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(FoodmenuComponent);
+    fixture = TestBed.createComponent(FoodMenuComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
