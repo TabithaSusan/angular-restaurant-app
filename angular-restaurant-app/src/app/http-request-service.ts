0cfd52764805a2f8e33cfed43f9a6525b2f34ce7
Friese2001 <104445528+Friese2001@users.noreply.github.com>
2022-07-19 14:59:31 +0200

    datenbank anbindung beispiel übernommen

---
+++ angular-restaurant-app/src/app/http-request-service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs";
+
+@Injectable ({
+    providedIn : 'root'
+})
+
+export class HttpRequestService {
+    constructor( private httpClient : HttpClient) { }
+
+    getGerichte():Observable<any>
+    {
+        return this.httpClient.get('gerichte');
+    }
+}
