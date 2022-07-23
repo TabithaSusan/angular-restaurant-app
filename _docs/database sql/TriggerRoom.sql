f207d4a2cd6cedcddbe7eba81ddffffd5cb0e996
51pennys <s51pennys@gmail.com>
2022-07-23 16:41:41 +0200

    trigger steht

--- _docs/database sql/TriggerRoom.sql
+++ _docs/database sql/TriggerRoom.sql
@@ -2,12 +2,8 @@ Delimiter $$
 
 CREATE TRIGGER roomLimit BEFORE INSERT ON R_gebuchtVon FOR EACH ROW
      BEGIN
-        IF (SELECT COUNT (Raum_id) from R_gebuchtVon) = 0 THEN
-			INSERT INTO R_gebuchtVon (Raum_id, Kunden_id) values (new.Raum_id, new.Kunden_id);
-		ELSEIF (SELECT COUNT (Raum_id) from R_gebuchtVon) < 4 THEN
-			INSERT INTO R_gebuchtVon (Raum_id, Kunden_id) values (new.Raum_id, new.Kunden_id);
-		ELSEIF (SELECT COUNT (Raum_id) from R_gebuchtVon) >= 4 THEN
-			set msg = concat('Fehler bei der Raumbuchung ', cast(new.id as char));
-			signal sqlstate '45000' set message_text = msg;
+        IF (SELECT COUNT(*) from R_gebuchtVon) > 4 THEN
+            set msg = concat('Fehler bei der Raumbuchung ');
+            signal sqlstate '45000' set message_text = msg;
          END IF;
     END $$
