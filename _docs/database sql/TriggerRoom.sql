3b92d085fac7e6ff49fac766eb9dec070ab87295
51pennys <s51pennys@gmail.com>
2022-07-23 16:58:00 +0200

    Trigger

--- _docs/database sql/TriggerRoom.sql
+++ _docs/database sql/TriggerRoom.sql
@@ -3,7 +3,7 @@ Delimiter $$
 CREATE TRIGGER roomLimit BEFORE INSERT ON R_gebuchtVon FOR EACH ROW
      BEGIN
         IF (SELECT COUNT(*) from R_gebuchtVon) > 4 THEN
-            set msg = concat('Fehler bei der Raumbuchung ');
-            signal sqlstate '45000' set message_text = msg;
+            SET msg = concat('Fehler bei der Raumbuchung ');
+            signal sqlstate '45000' SET message_text = msg;
          END IF;
     END $$
