9c07addc3309bfffcb958b1f7aef5a7966be5d44
51pennys <s51pennys@gmail.com>
2022-07-24 21:34:07 +0200

    errors

--- lechaudron-app/src/app/global-error-handler.service.ts
+++ lechaudron-app/src/app/global-error-handler.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { ErrorHandler, Injector} from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GlobalErrorHandlerService {
+export class GlobalErrorHandlerService implements ErrorHandler {
 
-  constructor() { }
+  constructor(private injector: Injector) { }
+  
+  
+  handleError(error: any): void {
+    let router = this.injector.get(Router);
+    console.log('URL: ' + router.url);
+    console.error('Ein Fehler ist aufgetreten', error.message);
+    alert(error);
+}
+  reserveTableError(reserveTableError: any): void {
+    let router = this.injector.get(Router);
+  console.log('URL: ' + router.url);
+  console.error(reserveTableError);
+  router.navigate(['/reserveTableError']);
+  }
+  bookRoomError(bookRoomError: any): void {
+    let router = this.injector.get(Router);
+    console.log('URL: ' + router.url);
+    console.error(bookRoomError);
+    router.navigate(['/bookRoomError']);
+  }
+  cartToFull(cartToFull: any): void {
+    let router = this.injector.get(Router);
+    console.log('URL: ' + router.url);
+    console.error(cartToFull);
+    router.navigate(['/foodOrderCartTooFull']);
+  }
+  foodOrderKitchenToFull(foodOrderKitchenToFull: any): void {
+    let router = this.injector.get(Router);
+    console.log('URL: ' + router.url);
+    console.error(foodOrderKitchenToFull);
+    router.navigate(['/foodOrderKitchenTooFull']);
+  }
 }
